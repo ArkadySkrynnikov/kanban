803e44cf86bfb3a7f25681efb6e3c5434b18d17a
ArkadySkrynnikov <catmaster1488@gmail.com>
2023-11-06 15:50:34 +0700

    add link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,14 @@
 import { ReactElement } from "react";
 import style from "./header.module.scss";
 import UserAvatar from "./userAvatar/userAvatar.tsx";
+import {Link} from "react-router-dom";
 
 function Header(): ReactElement {
     return (
         <>
             <header className={style.header_container}>
                 <div className={style.header_container__content}>
-                    <span className={style.header_logo}>Awesome Kanban Board</span>
+                    <Link to={"/"} className={style.link}><span className={style.header_logo}>Awesome Kanban Board</span></Link>
                     {/*<button onClick={()=>localStorage.clear()}>Clear</button>*/}
                     <UserAvatar />
                 </div>
